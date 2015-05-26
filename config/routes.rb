caba65cec83b0e20b002b4ede9e1cc60fd487ed4
michele <michelesaladino@hotmail.com>
2015-05-26 10:42:18 +0200

    Remove Show and Index review URLs

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,9 @@
 Rails.application.routes.draw do
-  
-  resources :reviews
-  resources :reviews
+  resources :reviews, except: [:show, :index]
+ 
   devise_for :users
-  resources :restaurants do
-    resources :reviews, except: [:show, :index]
-end
+  resources :restaurants 
+
 
   get 'pages/about'
 
